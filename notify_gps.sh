d5380ab619938c763323c025e756607ed44286f0
lionee <czerniawski.daniel@gmail.com>
2016-01-27 12:30:14 +0100

    New file: notify_gps.sh

---
+++ notify_gps.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+OLD=$(cat files_count.txt | sed 's|[^0-9]||g' | bc -l) 
+ACTUAL=`ls -l trips | wc -l`
+echo $ACTUAL
+if [ $ACTUAL -gt $OLD ]; then
+    echo "Nowy plik"
+    echo $ACTUAL > files_count.txt
+    /Applications/Utilities/terminal-notifier.app/Contents/MacOS/terminal-notifier -title "GPS tracking" -subtitle "Auto ruszyło" -message "Dane zapisują się w /Users/ja/trips"
+    mail -s "BMW w ruchu" daniel@czerniawski.org < email.txt
+else
+    echo "Brak nowych plików"
+fi
